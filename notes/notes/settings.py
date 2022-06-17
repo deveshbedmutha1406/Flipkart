ee0c4d4ee34ea33b861b9790b5d8c8868b2705c5
deveshbedmutha1406 <deveshbedmutha500@gmail.com>
2022-06-17 11:16:48 +0530

    third

--- notes/notes/settings.py
+++ notes/notes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1j#k3&v0%b%f)*7dz@xr-h$x@teu(l_11fwse1#)dex*t(kbd$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.0.1',"notesapp1234.herokuapp.com"]
 
 
 # Application definition
